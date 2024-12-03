59cf15c26a1df7ca8913ce0ccea6756f6085cc4e
David Turchak <david.turchak@gmail.com>
2024-12-03 16:30:04 +0200

    added validations and prints to azure_vm_avalability_script

--- azure_vm_avalability_script.sh
+++ azure_vm_avalability_script.sh
@@ -9,15 +9,15 @@ usage() {
   cat <<EOF
 Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] -r eastus -s Standard_D2s_v3 -n 1
 
-+-----------------------------------------------------------------------------------------------+
-| This script is intended to test virtual machine creation in a specified region.              |
-| It retrieves the list of zones within the region, creates the necessary resources in         |
-| each zone, and performs cleanup by deleting the created resources.                           |
-| Finally, it displays the success or failure results for each zone.                           |
-|                                                                                              |
-| Before running the script, ensure that the Azure CLI environment is preconfigured, including |
-| the default subscription, account, and required permissions!                                 |
-+-----------------------------------------------------------------------------------------------+
++-----------------------------------------------------------------------------------------------+ 
+| This script is intended to test virtual machine creation in a specified region.              | 
+| It retrieves the list of zones within the region, creates the necessary resources in         | 
+| each zone, and performs cleanup by deleting the created resources.                           | 
+| Finally, it displays the success or failure results for each zone.                           | 
+|                                                                                              | 
+| Before running the script, ensure that the Azure CLI environment is preconfigured, including | 
+| the default subscription, account, and required permissions!                                 | 
++-----------------------------------------------------------------------------------------------+ 
 
 Available options:
 
@@ -52,6 +52,18 @@ die() {
   local code=${2:-1}
   msg "$msg"
   exit "$code"
+}
+
+validate_jq() {
+  if ! command -v jq &> /dev/null; then
+    die "jq is not installed. Please install jq to proceed."
+  fi
+}
+
+validate_az() {
+  if ! command -v az &> /dev/null; then
+    die "az-cli is not installed. Please install az-cli to proceed."
+  fi
 }
 
 parse_params() {
@@ -104,26 +116,21 @@ print_table() {
         print "VMSize\t\tZone\tCreatedVMs\tFailedVMs"
     }
     {
-        # Remove commas from the input data
         gsub(",", "", $0)
-
-        # Extract fields
         for (i = 1; i <= NF; i++) {
             if ($i ~ /^VMSize:/) vm_size = $(i+1)
             if ($i ~ /^Zone:/) zone = $(i+1)
             if ($i ~ /^Successfully/) created_vms = $(i+2)
             if ($i ~ /^Failed/) failed_vms = $(i+3)
         }
-
-        # Print the results in the required format
         print vm_size "\t" zone "\t" created_vms "\t" failed_vms
     }' "$input_file" | column -t
 }
 
-
 create_resource_group() {
   local resource_group=$1
   local region=$2
+  msg "Creating resource group '$resource_group' in region '$region'..."
   az group create --name "$resource_group" --location "$region" --output tsv
 }
 
@@ -131,6 +138,7 @@ create_vnet() {
   local vnet_name=$1
   local resource_group=$2
   local subnet_name=$3
+  msg "Creating virtual network '$vnet_name' in resource group '$resource_group'..."
   az network vnet create \
     --name "$vnet_name" \
     --resource-group "$resource_group" \
@@ -148,6 +156,7 @@ create_vm() {
   local vnet_name=$5
   local subnet_name=$6
   local zone=$7
+  msg "Creating VM '$vm_name' in region '$region', zone '$zone'..."
   az vm create \
     --resource-group "$resource_group" \
     --name "$vm_name" \
@@ -163,6 +172,8 @@ create_vm() {
 }
 
 main() {
+  validate_jq
+  validate_az
   parse_params "$@"
   setup_colors
 
@@ -170,37 +181,41 @@ main() {
   vnet_name="test-vnet-${region}"
   subnet_name="test-subnet-${region}"
 
+  msg "Creating resource group and virtual network..."
   create_resource_group "$resource_group" "$region"
   create_vnet "$vnet_name" "$resource_group" "$subnet_name"
   
- 
+  msg "Fetching available zones for region '$region' and VM size '$size'..."
   readarray -t zones < <(az vm list-skus --location "$region" --size "$size" --output json | jq -r '.[0].locationInfo[0].zones[]')
 
   rm -f /tmp/test_zones.log
   for zone in "${zones[@]}"; do
-  unset success_count failure_count job_statuses
-  success_count=0
-  failure_count=0
-  declare -A job_statuses
+    unset success_count failure_count job_statuses
+    success_count=0
+    failure_count=0
+    declare -A job_statuses
 
-  for i in $(seq 1 "$number_of_vms"); do
-    vm_name="vm-${region}-z${zone}-${i}"
-    create_vm "$vm_name" "$resource_group" "$region" "$size" "$vnet_name" "$subnet_name" "$zone" &
-    job_statuses[$!]="$vm_name"
+    msg "Starting VM creation in zone '$zone'..."
+    for i in $(seq 1 "$number_of_vms"); do
+      vm_name="vm-${region}-z${zone}-${i}"
+      create_vm "$vm_name" "$resource_group" "$region" "$size" "$vnet_name" "$subnet_name" "$zone" &
+      job_statuses[$!]="$vm_name"
+    done
+
+    msg "Waiting for all VMs to be created..."
+    for job in "${!job_statuses[@]}"; do
+        if wait "$job"; then
+            success_count=$((success_count + 1))
+        else
+            failure_count=$((failure_count + 1))
+        fi
+    done
+
+    msg "Finished VM creation in zone '$zone'. Success: $success_count, Failure: $failure_count."
+    echo "VMSize: $size, Zone: $zone, Successfully created: $success_count, Failed to create: $failure_count" >> /tmp/test_zones.log
   done
 
-  # Wait for all background jobs to complete
-  for job in "${!job_statuses[@]}"; do
-      if wait "$job"; then
-          success_count=$((success_count + 1))
-      else
-          failure_count=$((failure_count + 1))
-      fi
-  done
-
-  echo "VMSize: $size, Zone: $zone, Successfully created: $success_count, Failed to create: $failure_count" >> /tmp/test_zones.log
-done
-
+  msg "Cleaning up by deleting resource group '$resource_group'..."
   az group delete --name "$resource_group" --yes --no-wait --output tsv
   print_table "/tmp/test_zones.log"
 }
